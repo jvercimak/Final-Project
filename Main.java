f53e3dc75ccd7e7b3abdbf6cae8e210ab823b754
jvercimak <46680682+jvercimak@users.noreply.github.com>
2019-12-08 20:31:00 -0600

    Main

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ */
+public class Main {
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        /**
+         * A main app that launches the program by creating and showing the
+         * form. (2 points)
+         */
+
+        GUI gui = new GUI();
+
+    }
+
+}
